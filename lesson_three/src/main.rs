2d34faf5d83333043746b76735c19053966e0234
samuel <jakeben8990@gmail.com>
2024-01-01 16:13:54 +0100

    wrote a recursive function

--- lesson_three/src/main.rs
+++ lesson_three/src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 fn process_numbers(sum: i32) {
     println!("Sum = {sum}");
 
@@ -22,9 +24,27 @@ fn sum_number(nums: &[i32]) -> i32 {
     sum
 }
 
+fn fib(n_index: i32) -> i32 {
+    if n_index <= 1 {
+        n_index
+    } else {
+        fib(n_index - 1) + fib(n_index - 2)
+    }
+}
+
 fn main() {
     let sum = sum_number(&[1, 7, 4, 9, 2, 3]);
     process_numbers(sum);
     let chunk = split_string("hello,world".to_string(), ',', 1);
-    println!("Split string : {chunk}")
+    println!("Split string : {chunk}");
+
+    println!("Finding the fib for an index");
+    println!("Input the fib index you want to find");
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to readline");
+    let fib_index = input.trim().parse().expect("Please enter a number");
+    let result = fib(fib_index);
+    println!("Fib of {fib_index} is = {result}")
 }
