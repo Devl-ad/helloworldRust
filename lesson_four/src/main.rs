cceee361d01cdc18f9db0df413f37d36bbb0ba7d
samuel <jakeben8990@gmail.com>
2024-01-02 10:46:24 +0100

    ownership

--- lesson_four/src/main.rs
+++ lesson_four/src/main.rs
@@ -1,3 +1,36 @@
+// The ownership concept
+
+// fn take_ownership(str: String) {
+//     println!("{} world", str);
+// }
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
 fn main() {
-    println!("Hello, world!");
+    //name is not valid here
+    {
+        let name = "John";
+        println!("{name}")
+    } //name goes out of scope
+      // println!("{name}");
+    let x = 5;
+    let mut y = x; //copy
+
+    y += 8;
+
+    println!("x = {x} and y = {y}");
+
+    let s1: String = String::from("hello");
+    // let s2 = s1;
+
+    // println!("{},{}", s1, s2)
+    // println!("{}", s2)
+    // take_ownership(s1);
+
+    // let len = calculate_length(s1);
+    // println!("{} world ,len={}", s1, len)
+
+    let len = calculate_length(&s1);
+    println!("{} world ,len={}", s1, len)
 }
